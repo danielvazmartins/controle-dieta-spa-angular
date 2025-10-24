0ecb3fb3487613527e4f5f40c481382611a00fb6
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2025-10-24 16:56:17 -0300

    feat: desenvolvimento aplicação - em andamento

--- src/app/home/meal/meal.component.ts
+++ src/app/home/meal/meal.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-meal",
   templateUrl: "./meal.component.html",
-  styleUrl: "./meal.component.scss"
+  styleUrl: "./meal.component.scss",
+  imports: [
+    CommonModule
+  ],
 })
 export class MealComponent { 
-
+  @Input() meal: any;
 }
--- src/app/shared/mocks/diets.mock.ts
+++ src/app/shared/mocks/diets.mock.ts
@@ -7,7 +7,16 @@ export const DIETS_MOCK = [
         meals: [
             {
                 id: 1,
-                name: 'Café da Manhã'
+                name: 'Café da Manhã',
+                items: [
+                    { name: 'Whey protein concentrado', calories: 120, quantity: 1, unit: 'scoop' },
+                    { name: 'Aveia em flocos', calories: 150, quantity: 40, unit: 'g' },
+                    { name: 'Banana', calories: 105, quantity: 1, unit: 'unidade' },
+                    { name: 'Castanha do Pará', calories: 200, quantity: 30, unit: 'g' },
+                    { name: 'Cacau em pó 100%', calories: 12, quantity: 5, unit: 'g' },
+                    { name: 'Psyllium', calories: 20, quantity: 10, unit: 'g' },
+                    { name: 'Leite desnatado', calories: 90, quantity: 200, unit: 'ml' }
+                ]
             }, {
                 id: 2,
                 name: 'Almoço'
