853f1f70f10919d49db67b8e68156636e78f76bd
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2026-06-14 19:53:07 -0300

    feat: total de calorias

--- src/app/features/home/total/total.component.ts
+++ src/app/features/home/total/total.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-total",
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   styleUrl: "./total.component.scss"
 })
 export class TotalComponent {
-
+  @Input() total: any;
 }
